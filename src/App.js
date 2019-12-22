732a15495f6d7f5360b66963be8f2a422c516675
Tomasz Biłas <tomasz.bilas@gmail.com>
2019-12-22 14:58:32 +0100

    add main component, set routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core'
+
+import LoginPage from './components/LoginPage'
+import Home from './components/Home'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1
+  },
+  link: {
+    textDecoration: 'none'
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
+      <Router>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title} >
+              Image Viewer
+            </Typography>
+            <Button component={Link} to="/login" color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      
+        <Route exact path="/" component={Home} />
+        <Route path="/login" render={props => <LoginPage {...props} />} />
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
