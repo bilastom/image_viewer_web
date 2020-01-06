82adb2041b8c692d728ec13b9c64200b1fe81240
Tomasz Biłas <tomasz.bilas@gmail.com>
2020-01-06 19:42:08 +0100

    improve grid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux"
+
+import {
+  Link
+} from "react-router-dom";
+
 import Modal from './UploadModal'
 import { getImages } from "../actions"
-import { 
+import {
   GridListTile, 
   GridListTileBar, 
 } from "@material-ui/core"
@@ -30,17 +35,19 @@ class Home extends Component {
   render = () => {
     return(
       <div style={styles.container}>
-        <Modal />
-        {this.props.images.map(image => (
-          <div style={styles.image}>
-            <GridListTile key={image.thumbnail_url}>
-              <img src={image.thumbnail_url} alt={image.filename} />
-              <GridListTileBar
-                title={image.filename}
-              />
-            </GridListTile>
-          </div>
-        ))}
+        <Modal/>
+          {this.props.images.map(({id, filename, thumbnail_url}) => (
+            <Link key={id} to={`/images/${id}`} state={{filename }}>
+              <div style={styles.image}>
+                <GridListTile>
+                  <img src={thumbnail_url} alt={filename} />
+                  <GridListTileBar
+                    title={filename}
+                  />
+                </GridListTile>
+              </div>
+            </Link>
+          ))}
       </div>
     )
   }
